e7a1a1bef1bbf4f3778717ab56e0df2d12c5d44d
Inácio Oliveira <inacioigne@gmail.com>
2022-05-09 17:22:25 -0400

    Inicio

---
+++ pages/posts/first-post.js
@@ -0,0 +1,40 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
+
+
+export default function FirstPost() {
+    return (
+        <Layout>
+        <Head>
+        <title>First Post</title>
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      </Head>
+        <h1>First Post</h1>
+        <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+      <YourComponent />
+
+        </Layout>
+    )
+  }
